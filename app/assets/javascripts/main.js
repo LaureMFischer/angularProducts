3c6d1f0a9e369245edce53c09984e9531e3474ad
Laure Fischer <laurefischer27@gmail.com>
2014-03-11 16:11:42 -0400

    completed all steps in steps txt file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,23 @@
 // TODO: Add angular application module and routes.
+//= require_self
+//= require_tree ./services/main
+//= require_tree ./filters/main
+//= require_tree ./controllers/main
+//= require_tree ./directives/main
+
+var StoreFront = angular.module('StoreFront',['ngRoute']);
+
+StoreFront.config(['$routeProvider', function($routeProvider){
+  // Default Route
+  $routeProvider.otherwise({
+      templateUrl: '../assets/mainIndex.html',
+      controller: 'IndexCtrl'
+  });
+
+  // Route to retrieve one product
+  // '/product/:productId
+  $routeProvider.when('/product/:productId',{
+      templateUrl: '../assets/mainProduct.html',
+      controller: 'ProductCtrl'
+  });
+}]);
